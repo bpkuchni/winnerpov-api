3fdfd1e95c253c4c12d533476ea8e3ae5861be59
Brandon Kuchnicki <bpkuchni@gmail.com>
2024-03-15 10:36:38 -0400

    Add project files.

---
+++ WinnerPOV-API/Program.cs
@@ -0,0 +1,36 @@
+namespace WinnerPOV_API
+{
+    public class Program
+    {
+        //https://app.swaggerhub.com/apis-docs/Henrik-3/HenrikDev-API/3.0.0#/
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
