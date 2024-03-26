f41af9a42e2186ec36b01cdcf2db6d38dc4b6ab2
Brandon Kuchnicki <bpkuchni@gmail.com>
2024-03-26 14:04:33 -0400

    Add CORS

--- WinnerPOV-API/Program.cs
+++ WinnerPOV-API/Program.cs
@@ -10,11 +10,23 @@ namespace WinnerPOV_API
         //https://app.swaggerhub.com/apis-docs/Henrik-3/HenrikDev-API/3.0.0#/
         public static void Main(string[] args)
         {
+
+
+            var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+
             var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddCors(options =>
+            {
+                options.AddPolicy(name: MyAllowSpecificOrigins, policy =>
+                {
+                    policy.WithOrigins("http://winnerpov.brandonkuchnicki.net", "http://winnerpov.brandonkuchnicki.com", "https://winnerpov.brandonkuchnicki.net", "http://winnerpov.brandonkuchnicki.com");
+                });
+            });
 
             // Add services to the container.
             builder.Services.AddControllers();
-            
+
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
@@ -25,12 +37,14 @@ namespace WinnerPOV_API
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
-        
-                app.UseSwagger();
-                app.UseSwaggerUI();
- 
+
+            app.UseSwagger();
+            app.UseSwaggerUI();
+
 
             app.UseHttpsRedirection();
+
+            app.UseCors();
 
             app.UseAuthorization();
 
